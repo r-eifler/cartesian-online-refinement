645ab9e0d9629bcfc9dd105fe6167cd79d55c5a0
Rebecca Eifler <s9reeifl@stud.uni-saarland.de>
2018-08-10 16:03:03 +0200

    split deviation original rule

--- src/search/cegar/abstraction.cc
+++ src/search/cegar/abstraction.cc
@@ -1516,14 +1516,16 @@ unique_ptr<Flaw> Abstraction::find_flaw(const Solution &solution, AbstractState 
                     cout << "  Paths deviate." << endl;
                 ++deviations;
                 //cout << "path deviate: " << length_correct_trace << "/" << solution.size() << endl;
+				/*
                 return utils::make_unique_ptr<Flaw>(
                     move(concrete_state),
                     abstract_state,
                     refinement_hierarchy.get_node(next_concrete_state)->get_AbstractState()->regress(op));
-                //return utils::make_unique_ptr<Flaw>(
-                //    move(concrete_state),
-                //    abstract_state,
-                //    next_abstract_state->regress(op));
+				*/
+                return utils::make_unique_ptr<Flaw>(
+                    move(concrete_state),
+                    abstract_state,
+                    next_abstract_state->regress(op));
             }
             abstract_state = next_abstract_state;
             concrete_state = move(next_concrete_state);
